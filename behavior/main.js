029fffe96da96229e10243b59b7c14d84c93c855
Agustín <agustin.golmar@gmail.com>
2020-08-28 03:15:01 -0300

    More accounts and tooltips. Fragments for tab links.

--- behavior/main.js
+++ behavior/main.js
@@ -2,6 +2,13 @@
 
 (function (_document, _$) {
 	_$(function () {
-		// ...
+		_$("[data-toggle = 'tooltip']").tooltip();
+		const URL = _document.location.toString();
+		if (URL.match("#")) {
+			_$(".list-group a[href='#" + URL.split("#")[1] + "']").tab("show");
+		}
+		_$("#nav-viz a").click(function (event) {
+			parent.location.hash = "#" + this.href.split("#")[1];
+		});
 	});
 })(document, $);
